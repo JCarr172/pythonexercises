b7d87cd0ea920c08660368a16e299cafafb4deb4
James C <james.carr@QA.co.uk>
2021-04-22 15:46:37 +0000

    Updated Exercise5.py for finished product

--- Exercise5.py
+++ Exercise5.py
@@ -1,7 +1,19 @@
 x = input('Please enter numbers seperated by spaces')
 x = x.split(' ')
-print(x)
-if x.isdigit() == True:
-    pass
+
+xint = []
+N = 0
+for i in range(0,len(x)-1):
+    if x[i].isdigit() == True:
+        xint.append(int(x[i]))
+    else:
+        N = 1
+
+if N == 1:
+    print('Not everything was a number')
 else:
-    print("They weren't all numbers")
+    print(xint[0], xint[-1])
+    print(xint + xint)
+    print(list(reversed(xint)))
+    xint.sort()
+    print(list(reversed(xint)))
